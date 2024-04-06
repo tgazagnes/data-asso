a0f2b7536502ee0739e9c383db0ff1a397bea6c8
Thibaut Gazagnes <122997528+tgazagnes@users.noreply.github.com>
2024-04-06 11:29:10 +0200

    treemap_test

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import os
 #from streamlit_dynamic_filters import DynamicFilters
 
 # Page setting : wide layout
@@ -24,28 +23,30 @@ st.markdown(
 Analyses des subventions accordées aux acteurs associatifs en 2022 (territoire : Métropole de Lyon)
 """
 )
+@st.cache_data
+def load_data():
+    df = pd.read_parquet("data/base_consolidee.parquet")
+    # Copie des données pour transfo
+    df_total = df.copy()
+    return df_total
 
-df = pd.read_parquet("data/base_consolidee.parquet")
-
-# Copie des données pour transfo
-df_total = df.copy()
+df_total = load_data()
 
 # Création du filtre dynamique par niveau géographique
 #niveaux_geo = ["REGION", "DEP_CODE_NOM", "LIBEPCI", "BASSIN_DE_VIE", "COMMUNE_CODE_NOM"]
 #dynamic_filters = DynamicFilters(df_other, filters=niveaux_geo)
 #df_other_filtre = dynamic_filters.filter_df()
 
-tab1, tab2 = st.tabs(
+tab1, tab2, tab3= st.tabs(
     [
         "Statistiques d'ensemble :trophy:",
-        "Top subventions :microscope:"
+        "Top subventions :microscope:",
+        "Treemap subventions Etat :evergree_tree:"
     ]
 )
 
 
 with tab1:
-
-
 
     # Calcul des indicateurs clés de haut de tableau avant transformation
     nb_benef = df_total["Bénéficiaire"].nunique()
@@ -55,6 +56,14 @@ with tab1:
     df_financeur_benef = df_total.groupby(
         ["Financeur", "Bénéficiaire"], 
         as_index = False)["Montant"].sum()
+
+
+    #preparation des données
+    df_financeur_global = df_financeur_benef.groupby("Financeur", as_index = False).agg({
+        "Bénéficiaire" : "count",
+        "Montant" : "sum"
+    })
+    df_financeur_global["Montant_2"] = df_financeur_global["Montant"].map('{:,.0f}'.format).str.replace(","," ") + " €"
 
     # Charte graphique MERTERRE :
     colors_map = {
@@ -85,7 +94,7 @@ with tab1:
     # 2ème métrique 
     cell2 = l1_col2.container(border=True)
     montant_total_2022 = f"{montant_total_2022:,.0f}".replace(",", " ") + " €"
-    cell2.metric("Montant total alloué en 2022", f"{montant_total_2022}")
+    cell2.metric("Montant total alloué en 2022 (tous financeurs)", f"{montant_total_2022}")
 
 
     # Ligne 2 : 2 graphiques en ligne : donut et bar chart matériaux
@@ -93,12 +102,6 @@ with tab1:
 #    with l2_col1:
 
     #Graphique à barres empliées du nombre de déchets par type
-
-    df_financeur_global = df_financeur_benef.groupby("Financeur", as_index = False).agg({
-        "Bénéficiaire" : "count",
-        "Montant" : "sum"
-    })
-    df_financeur_global["Montant_2"] = df_financeur_global["Montant"].map('{:,.0f}'.format).str.replace(","," ") + " €"
 
 
     fig = px.bar(df_financeur_global.sort_values(by = "Montant", ascending = False), x = "Montant", y="Financeur", 
@@ -113,6 +116,7 @@ with tab1:
         uniformtext_minsize=8,
         uniformtext_mode="hide",
         showlegend=False,
+        yaxis_title=None
     )
     # Affichage du graphique
     st.plotly_chart(fig, use_container_width=True)
@@ -132,7 +136,8 @@ with tab1:
         uniformtext_minsize=8,
         uniformtext_mode="hide",
         showlegend=False,
-        xaxis_title="Nombre de structures bénéficiaires"
+        xaxis_title="Nombre de structures bénéficiaires",
+        yaxis_title = None
     )
 
     # Affichage du graphique
@@ -177,6 +182,18 @@ with tab2:
         uniformtext_mode="hide",
         showlegend=False,
         xaxis_title="Montant des subventions accordées en 2022",
-        yaxis=dict(autorange="reversed")
+        yaxis=dict(autorange="reversed"),
+        yaxis_title = None
     )
+    st.plotly_chart(fig, use_container_width=True)
+
+with tab3:
+    df_financeur_etat = df_total[df_total["Financeur"] == "Etat"].groupby(["Bénéficiaire"], as_index = False)["Montant"].sum().sort_values(by = "Montant", ascending=False)
+    fig = px.treemap(df_financeur_etat, path=[px.Constant("Etat")], values='Montant',
+                    color='Bénéficiaire', 
+#                    hover_data=['iso_alpha'],
+                    color_continuous_scale='RdBu',
+#                    color_continuous_midpoint=np.average(df['lifeExp'], weights=df['pop'])
+    )
+    fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
     st.plotly_chart(fig, use_container_width=True)
